8e6e1d1c1389496b1bdf16ab5d18e2d55f275b8f
hmanimaran <hmanimaran@localhost.localdomain>
2024-12-06 10:40:04 +0530

    Initial commit

---
+++ module1.py
@@ -0,0 +1,12 @@
+def add(a, b):
+    """
+    Add two numbers.
+
+    Args:
+        a (int): First number.
+        b (int): Second number.
+
+    Returns:
+        int: Sum of the numbers.
+    """
+    return a + b
---
+++ module2.py
@@ -0,0 +1,11 @@
+def greet(name):
+    """
+    Greet a user.
+
+    Args:
+        name (str): Name of the user.
+
+    Returns:
+        str: Greeting message.
+    """
+    return f"Hello, {name}!"
